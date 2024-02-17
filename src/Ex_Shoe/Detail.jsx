c35f3c6e4400466e0dc95439a01a2f98b8a60583
Trần Tuấn Anh <anhtt@hvttgroup.com>
2024-02-17 13:59:30 +0700

    Half
    Half

---
+++ src/Ex_Shoe/Detail.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class Detail extends Component {
+  render() {
+    let name = this.props.name;
+    return <div className="col-5">
+        <img src="" alt="" />
+        <h3>{name}</h3>
+    </div>;
+  }
+}
+
+// rcc
